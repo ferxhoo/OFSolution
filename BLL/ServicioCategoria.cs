dc3ffe92bd86b9fbfcdd0fe8eb9a1361725a0faa
ferxhoo <jfflorez@unicesar.edu.co>
2024-06-01 10:47:12 -0500

    Logica de Negocio y de Datos de el modulo de Categoria

---
+++ BLL/ServicioCategoria.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ServicioCategoria
+    {
+        private DataCategoria dataCategoria = new DataCategoria();
+
+        public List<Categoria> Listar()
+        {
+            return dataCategoria.Listar();
+        }
+
+        public int Registrar(Categoria obj, out string Mensaje)
+        {    
+            return dataCategoria.Registrar(obj, out Mensaje);
+        }
+
+        public bool Editar(Categoria obj, out string Mensaje)
+        {
+            return dataCategoria.Editar(obj, out Mensaje);
+        }
+
+        public bool Eliminar(Categoria obj, out string Mensaje)
+        {
+            return dataCategoria.Eliminar(obj, out Mensaje);
+        }
+    }
+}
---
+++ DAL/DataCategoria.cs
@@ -0,0 +1,173 @@
+﻿using ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataCategoria
+    {
+
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+            {
+
+                try
+                {
+
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("SELECT idCategoria, descripcion, estado FROM CATEGORIAS");
+                    SqlCommand comando = new SqlCommand(query.ToString(), conexion);
+                    comando.CommandType = CommandType.Text;
+
+                    conexion.Open();
+
+                    using (SqlDataReader reader = comando.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            lista.Add(new Categoria()
+                            {
+                                idCategoria = Convert.ToInt32(reader["idCategoria"]),
+                                descripcion = reader["descripcion"].ToString(),
+                                estado = Convert.ToBoolean(reader["estado"])
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    lista = new List<Categoria>();
+                }
+            }
+
+            return lista;
+
+        }
+
+        public int Registrar(Categoria nuevaCategoria, out string mensaje)
+        {
+            int idCategoriaGenerado = 0;
+            mensaje = string.Empty;
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand comando = new SqlCommand("PROC_REGISTRARCATEGORIA", conexion);
+                    comando.Parameters.AddWithValue("descripcion", nuevaCategoria.descripcion);
+                    comando.Parameters.AddWithValue("estado", nuevaCategoria.estado);
+                    comando.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    comando.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+
+                    comando.ExecuteNonQuery();
+
+                    idCategoriaGenerado = Convert.ToInt32(comando.Parameters["resultado"].Value);
+                    mensaje = comando.Parameters["mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                idCategoriaGenerado = 0;
+                mensaje = ex.Message;
+            }
+
+            return idCategoriaGenerado;
+        }
+
+        public bool Editar(Categoria CategoriaEditada, out string mensaje)
+        {
+            bool respuesta = false;
+            mensaje = string.Empty;
+
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand comando = new SqlCommand("PROC_EDITARCATEGORIA", conexion);
+                    comando.Parameters.AddWithValue("idCategoria", CategoriaEditada.idCategoria);
+                    comando.Parameters.AddWithValue("descripcion", CategoriaEditada.descripcion);
+                    comando.Parameters.AddWithValue("estado", CategoriaEditada.estado);
+                    comando.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    comando.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+
+                    comando.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(comando.Parameters["resultado"].Value);
+                    mensaje = comando.Parameters["mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+
+        public bool Eliminar(Categoria categoria, out string mensaje)
+        {
+            bool respuesta = false;
+            mensaje = string.Empty;
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand comando = new SqlCommand("PROC_ELIMINARCATEGORIA", conexion);
+                    comando.Parameters.AddWithValue("idCategoria", categoria.idCategoria);
+                    comando.Parameters.Add("resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    comando.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+
+                    comando.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(comando.Parameters["resultado"].Value);
+                    mensaje = comando.Parameters["mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+
+    }
+
+
+}
+
